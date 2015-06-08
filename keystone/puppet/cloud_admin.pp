7f2830ca1df1b460182f21a62f5460c298f43917
Ajaya Agrawal <ajku.agr@gmail.com>
2015-06-08 09:22:27 +0000

    finished replacement of domain id in policy file

--- keystone/puppet/cloud_admin.pp
+++ keystone/puppet/cloud_admin.pp
@@ -5,6 +5,19 @@ file { 'cloud_admin_check':
   mode    => 755
 } ->
 
+file { 'policy.json':
+  path    => '/etc/keystone/policy.json',
+  content => template("/etc/puppet/templates/policy_json.erb"),
+  ensure  => 'file'
+} ->
+
+file { 'replace_domain_id_in_policy_file':
+  path    => '/home/vagrant/replace_domain_id_in_policy_file.sh',
+  content => template("/etc/puppet/templates/replace_domain_id_in_policy_file.erb"),
+  ensure  => file,
+  mode    => 755,
+} ->
+
 file { 'cloud_admin_create':
   path    => '/home/vagrant/cloud_admin_create.sh',
   content => template("/etc/puppet/templates/cloud_admin_create.erb"),
@@ -17,4 +30,10 @@ exec { 'create_cloud_admin':
  logoutput => true,
  command   => 'echo "cloud_admin is not present";cloud_admin_create.sh',
  unless   => 'cloud_admin_check.sh'
-}
+} -> 
+
+exec { 'replace_domain_id':
+ path      => '/bin:/usr/bin:/home/vagrant',
+ logoutput => true,
+ command   => 'replace_domain_id_in_policy_file.sh'
+} 
