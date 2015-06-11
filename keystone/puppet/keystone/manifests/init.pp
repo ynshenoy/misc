bd4fbd36b14858759bc3107796c7f2594327a5b0
Ajaya Agrawal <ajku.agr@gmail.com>
2015-06-11 10:45:10 +0000

    removed dependency among files

--- keystone/puppet/keystone/manifests/init.pp
+++ keystone/puppet/keystone/manifests/init.pp
@@ -11,40 +11,42 @@ class keystone(
     ensure  => 'file',
     path    => '/etc/keystone/policy.json',
     source => 'puppet:///modules/keystone/keystone_policy.json'
-  } ->
+  }
 
   file { 'replace_domain_id_in_policy_file':
     ensure  => 'file',
     path    => '/usr/local/bin/replace_domain_id_in_policy_file.sh',
     content => template('keystone/replace_domain_id_in_policy_file.erb'),
     mode    => 755,
-  } ->
+  }
 
   file { 'cloud_admin_check':
     ensure  => 'file',
     path    => '/usr/local/bin/cloud_admin_check.sh',
     content => template('keystone/cloud_admin_check.erb'),
     mode    => '0755'
-  } ->
+  }
 
   file { 'cloud_admin_create':
     ensure  => 'file',
     path    => '/usr/local/bin/cloud_admin_create.sh',
     content => template('keystone/cloud_admin_create.erb'),
     mode    => '0755'
-  } ->
+  }
 
   exec { 'cloud_admin_create':
     path      => '/bin:/usr/bin:/usr/local/bin',
     logoutput => true,
     command   => 'echo "cloud_admin is not present";cloud_admin_create.sh',
-    unless   => 'cloud_admin_check.sh'
+    unless   => 'cloud_admin_check.sh',
+    require   => [File['cloud_admin_check'], File['cloud_admin_create']]
   } ->
 
   exec { 'replace_domain_id':
     path      => '/bin:/usr/bin:/usr/local/bin',
     logoutput => true,
     command   => 'replace_domain_id_in_policy_file.sh',
-    onlyif    => 'cloud_admin_check.sh'
+    onlyif    => 'cloud_admin_check.sh',
+    require   => [File['cloud_admin_check'], File['policy.json'], File['replace_domain_id_in_policy_file']]
   }
 }
